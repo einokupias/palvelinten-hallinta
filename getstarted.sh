27730f43dae14438cf8becfa2c2ce265fed52160
Eino Kupias <kupias@hotmail.com>
2017-10-31 21:55:03 +0200

    added troll

--- getstarted.sh
+++ getstarted.sh
@@ -1,3 +1,28 @@
+echo "Never gonna give you up"
+sleep 1
+echo "Never gonna let you down"
+sleep 1
+echo "Never gonna run around and desert you"
+sleep 1
+echo "Never gonna make you cry"
+sleep 1
+echo "Never gonna say goodbye"
+sleep 1
+echo "Never gonna tell a lie and hurt you"
+sleep 5
+echo "Prepare for liftoff"
+sleep 1
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+
 setxkbmap fi
 timedatectl set-timezone Europe/Helsinki
 
@@ -10,7 +35,7 @@ git clone https://github.com/einokupias/palvelinten-hallinta.git
 
 sudo cp -r palvelinten-hallinta/puppet /etc/
 
-sudo puppet apply -e 'class {apache:}'
+sudo puppet apply -e 'class {apache:='
 
 
 
